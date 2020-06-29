77d63cafbf84b8aa521d245302c47c217ff0f87e
Emanuel Rodriguez <joseemaro@hotmail.com>
2020-06-29 15:29:25 -0300

    js y html validar enfermedades register ap

--- SegundaEntrega/InkMaster/public/js/validateAp.js
+++ SegundaEntrega/InkMaster/public/js/validateAp.js
@@ -51,4 +51,17 @@ document.addEventListener("DOMContentLoaded", function() {
             validate_date.style.border = "#ffffff";
         }
     });
+
+    //validar terminos
+    validate_pathology = document.querySelector(".pathologyjs");
+    validate_pathology.addEventListener("blur", function() {
+        var path = /^[a-zA-Z0-9+-.: ]{3,70}$/;
+        if (path.exec(validate_pathology.value) || (validate_pathology.value === "")) {
+            validate_pathology.style.border = "#ffffff";
+            validate_pathology.style.background = "#ffffff";
+        } else {
+            validate_pathology.style.background = "#e05f5f";
+        }
+    });
+
 });
