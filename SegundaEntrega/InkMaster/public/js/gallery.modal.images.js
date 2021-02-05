5ef20e1bd625414c1156d467f974fe8f10b48ec8
joseemaro <joseemaro@hotmail.com>
2021-02-05 18:57:12 -0300

    prueba de zoom en galeria

--- SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
+++ SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
@@ -3,20 +3,30 @@ var window = window || {},
     console = console || {};
 document.addEventListener("DOMContentLoaded", function() {
     // Get the modal
-    var modal = document.getElementsByClassName("myModal");
+    var modal = document.getElementById("myModal2");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementsByClassName("myImg")[0];
-    var modalImg = document.getElementsByClassName("img01");
-    var captionText = document.getElementsByClassName("caption");
-    img.addEventListener("click", function(event) {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    });
+    document.querySelectorAll('.myImg').forEach(item => {
+        item.addEventListener('click', event => {
+          //handle click
+            var contain = document.getElementsByClassName("container-img-artist")[0];
+            console.log(contain);
+            contain.removeAttribute("width");
+            contain.removeAttribute("margin");
+            contain.removeAttribute("display");
+            contain.removeAttribute("flex-wrap");
+            contain.removeAttribute("justify-content");
+            
+            item.style.display="block";
+            var modalImg = document.getElementById("img02");
+            modalImg.src = item.src;
+            var captionText = document.getElementById("caption2");
+            captionText.innerHTML = item.alt;
+        })
+      })
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close2")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
