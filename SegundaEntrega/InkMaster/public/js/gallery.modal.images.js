a631dd39496387ab9b079052a109a0d256fd8ec6
joseemaro <joseemaro@hotmail.com>
2021-02-13 14:05:39 -0300

    errores css y zoom funcionando en galerias

--- SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
+++ SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
@@ -3,30 +3,21 @@ var window = window || {},
     console = console || {};
 document.addEventListener("DOMContentLoaded", function() {
     // Get the modal
-    var modal = document.getElementById("myModal2");
+    var modal = document.getElementById("myModal");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     document.querySelectorAll('.myImg').forEach(item => {
         item.addEventListener('click', event => {
-          //handle click
-            var contain = document.getElementsByClassName("container-img-artist")[0];
-            console.log(contain);
-            contain.removeAttribute("width");
-            contain.removeAttribute("margin");
-            contain.removeAttribute("display");
-            contain.removeAttribute("flex-wrap");
-            contain.removeAttribute("justify-content");
-            
-            item.style.display="block";
-            var modalImg = document.getElementById("img02");
-            modalImg.src = item.src;
-            var captionText = document.getElementById("caption2");
-            captionText.innerHTML = item.alt;
+          var modalImg = document.getElementById("img01");
+          var captionText = document.getElementById("caption");
+          modal.style.display = "block";
+          modalImg.src = item.src;
+          captionText.innerHTML = item.alt;
         })
       })
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close2")[0];
+    var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
