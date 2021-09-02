2a0170362dfe9271c023c6f7252f64ad8b036bfb
joseemaro <joseemaro@hotmail.com>
2021-09-02 18:57:59 -0300

    FAQ-faqFilter-solved permision buttom problem

--- SegundaEntrega/InkMaster/public/ajax/buscarFaq.php
+++ SegundaEntrega/InkMaster/public/ajax/buscarFaq.php
@@ -45,26 +45,31 @@
 			$admin= false;
 			$aux = false;
 			if (isset($_SESSION["id_user"])) {
-				$admin = "select * from inkmaster_db.user as u
+/* 				$admin = "select * from inkmaster_db.user as u
 				inner join inkmaster_db.administrator as a on (u.id_user = a.id_administrator)
 				where a.id_local =1 and a.id_administrator =" . $_SESSION["id_user"] . "
-				and u.enabled is true";
+				and u.enabled is true"; */
+				$admin = "select * from inkmaster_db.administrator as u
+				where u.id_administrator ='" . $_SESSION["id_user"] . "'";
 				$aux = $conn->query($admin);
+
 			}
 			if ($aux){
-				$salida.= "<section id='section2-del'>
-				<form method='get' id='edit-faq-".$fila['id_faq']."' action='/edit_faq/".$fila['id_faq']."'>
-					<input type='hidden' name='id_faq' value=".$fila['id_faq'].">
-					<button class ='table-button editBtn' type='submit' form='edit-faq-".$fila['id_faq']."'>Edit</button>
-				</form>
-	
-				<form method='get' id='destroy-faq-".$fila['id_faq']."' action='/del_faq/".$fila['id_faq']."' onSubmit='return confirm('Desea eliminar la pregunta?');'>
-					<input type='hidden' name='id_faq' value=".$fila['id_faq'].">
-					<button class ='table-button deleteBtn' type='submit' form='destroy-faq-".$fila['id_faq']."'>Delete</button>
-				</form>
-			</section>
+				if ($aux->num_rows>0){
+					$salida.= "<section id='section2-del'>
+					<form method='get' id='edit-faq-".$fila['id_faq']."' action='/edit_faq/".$fila['id_faq']."'>
+						<input type='hidden' name='id_faq' value=".$fila['id_faq'].">
+						<button class ='table-button editBtn' type='submit' form='edit-faq-".$fila['id_faq']."'>Edit</button>
+					</form>
 		
-			";
+					<form method='get' id='destroy-faq-".$fila['id_faq']."' action='/del_faq/".$fila['id_faq']."' onSubmit='return confirm('Desea eliminar la pregunta?');'>
+						<input type='hidden' name='id_faq' value=".$fila['id_faq'].">
+						<button class ='table-button deleteBtn' type='submit' form='destroy-faq-".$fila['id_faq']."'>Delete</button>
+					</form>
+				</section>
+			
+				";
+				}
 			}
 			$salida.= "
 			</article>
