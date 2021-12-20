ad27b0c333804e603b0693d73a393d2a90859981
Melany Bulfero <melanybulfero97@gmail.com>
2021-12-20 18:35:46 -0300

    Feat: new nav

--- SegundaEntrega/InkMaster/public/js/base.js
+++ SegundaEntrega/InkMaster/public/js/base.js
@@ -1,6 +1,25 @@
-function cambiarClase(){
-    let siteNav = document.getElementById('site-nav');
-    siteNav.classList.toggle('site-nav-open');
-    let menuOpen = document.getElementById('menu-toggle');
-    menuOpen.classList.toggle('menu-open');
-}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const btnMenu = document.querySelector("#btnMenu");
+    const menu = document.querySelector("#menu");
+    btnMenu.addEventListener("click", function() {
+        menu.classList.toggle("show");
+    });
+
+    const subMenuBtn = document.querySelectorAll(".submenu-btn");
+    for ( let i = 0; i < subMenuBtn.length; i++ ) {
+        subMenuBtn[i].addEventListener("click", function() {
+            if ( window.innerWidth < 1024 ) {
+                const subMenu = this.nextElementSibling;
+                const height = subMenu.scrollHeight;
+                if ( subMenu.classList.contains("display") ) {
+                    subMenu.classList.remove("display");
+                    subMenu.removeAttribute("style");
+                } else {
+                    subMenu.classList.add("display");
+                    subMenu.style.height = height + "px";
+                }
+            }
+        });
+    }
+});
