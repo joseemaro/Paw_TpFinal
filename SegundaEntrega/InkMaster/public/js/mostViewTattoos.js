2c3ac4ecaa7638c78b6000135faa6b83d6fc901e
LucianoPalmieri <luciano.palmieri.97@gmail.com>
2021-12-26 23:28:55 -0300

    most view tattoo

--- SegundaEntrega/InkMaster/public/js/mostViewTattoos.js
+++ SegundaEntrega/InkMaster/public/js/mostViewTattoos.js
@@ -1,25 +1,30 @@
 var page = 2;
 
 window.addEventListener( 'scroll', () => {
-    if( window.scrollY + window.innerHeight >= document.documentElement.scrollHeight ){
+    var limit = document.documentElement.scrollHeight;
+    if( window.scrollY + window.innerHeight >= limit ){
         loadImages();
     }
+
 })
 
 function loadImages( val = 6 ) {
     var container = document.getElementById( "container" );
-    $.ajax( {
-        type: "POST",
-        url: "/public/ajax/loadImages.php",
-        dataType: "html",
-        data: {
-            val: val,
-            page: page
+    $.ajax(
+        {
+            type: "POST",
+            url: "/get_tattoos",
+            dataType: "html",
+            data: {
+                val: val,
+                page: page
+            },
         },
-    })
+        page = page + 1,
+    )
         .done( function( response ) {
             container.insertAdjacentHTML( 'beforeend', response );
-            page = page + 1;
+            // page = page + 1;
         } )
         .fail( function() {
             console.log( "error" );
