3a30592c26de7875f1db0912059c46736260a97e
joseemaro <joseemaro@hotmail.com>
2022-02-15 17:49:39 -0300

    faqfilter jquery to xhr & seo changes

---
+++ SegundaEntrega/InkMaster/public/js/faqFilterXhr.js
@@ -0,0 +1,60 @@
+
+this.addEventListener("DOMContentLoaded", () =>{
+    accordion();
+})
+
+function accordion() {
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        } else{
+            questions.forEach(question => question.parentNode.classList.remove("active"))
+            question.parentNode.classList.add("active")
+            var faq_id = question.getAttribute( "data-faq-id" );
+            increaseVisits( question, faq_id )
+        }
+    }))
+}
+
+function getFaqs( values ){
+
+    var http = new XMLHttpRequest();
+    var url = "/buscar_faq";
+    var params = JSON.stringify({ val: values });
+    var param = new FormData();
+    param.append('val', values);
+    http.open("POST", url, true);
+    
+    http.onreadystatechange = function (aEvt) {
+        if(http.readyState == 4 && http.status == 200) {
+            var node = document.getElementById( "content" );
+            node.parentNode.removeChild( node );
+            $( "#datos" ).html( http.responseText );
+            accordion();
+        }else{
+            console.log("error");
+        }
+    }
+    http.send(param);
+}
+
+function increaseVisits( question, faq_id ){
+    var req = new XMLHttpRequest();
+    var s = "/increase_faq/" + faq_id
+    req.open('GET', s, true);
+    req.onreadystatechange = function (aEvt) {
+
+    if (req.readyState == 4) {
+        if(req.status == 200){
+            var parentQuestion = question.parentElement,
+            visits = parentQuestion.querySelector('.visits');
+            visits.innerHTML = "Total de visitas: " + req.responseText;
+        }else{
+            console.log("error");
+        }
+        }
+    }
+req.send(null);
+}
+
