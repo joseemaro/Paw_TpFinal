d46bd8413f8eb76d2ba265ff7bd49fc774cd6f2c
joseemaro <joseemaro@hotmail.com>
2022-02-15 20:23:30 -0300

    faqfilter fix

--- SegundaEntrega/InkMaster/public/js/faqFilter.js
+++ SegundaEntrega/InkMaster/public/js/faqFilter.js
@@ -1,3 +1,8 @@
+
+this.addEventListener("DOMContentLoaded", () =>{
+    accordion();
+})
+
 function accordion() {
     const questions = document.querySelectorAll(".question")
     questions.forEach((question) => question.addEventListener("click", () =>{
@@ -12,42 +17,45 @@ function accordion() {
     }))
 }
 
-function getFaqs( val ){
-    $.ajax(
-        {
-            type: "POST",
-            url: "/buscar_faq",
-            dataType: "html",
-            data: {val: val},
-        }
-    )
-        .done( function( response ) {
+function getFaqs( values ){
+
+    var http = new XMLHttpRequest();
+    var url = "/buscar_faq";
+    var param = new FormData();
+    param.append('val', values);
+    http.open("POST", url, true);
+    
+    http.onreadystatechange = function (aEvt) {
+        if(http.readyState == 4 && http.status == 200) {
             var node = document.getElementById( "content" );
             node.parentNode.removeChild( node );
-            $( "#datos" ).html( response );
+            var target = document.getElementById("datos");
+            target.innerHTML += http.responseText;
             accordion();
-        } )
-        .fail( function() {
-            console.log("error");
-        } );
+        }
+    }
+    http.onerror = function() {
+        console.log( "error" );
+    };
+    http.send(param);
 }
 
 function increaseVisits( question, faq_id ){
-    $.ajax({
-        type: "GET",
-        url: "/increase_faq/" + faq_id,
-        dataType: "text"
-    })
-    .done(function( respuesta ){
-        var parentQuestion = question.parentElement,
+    var req = new XMLHttpRequest();
+    var s = "/increase_faq/" + faq_id
+    req.open('GET', s, true);
+    req.onreadystatechange = function (aEvt) {
+    if (req.readyState == 4) {
+        if(req.status == 200){
+            var parentQuestion = question.parentElement,
             visits = parentQuestion.querySelector('.visits');
-        visits.innerHTML = "Total de visitas: " + respuesta;
-    })
-    .fail(function(){
-        console.log("error");
-    });
+            visits.innerHTML = "Total de visitas: " + req.responseText;
+        }
+        }
+    }
+    req.onerror = function() {
+        console.log( "error" );
+    };
+req.send(null);
 }
 
-this.addEventListener("DOMContentLoaded", () =>{
-    accordion();
-})
