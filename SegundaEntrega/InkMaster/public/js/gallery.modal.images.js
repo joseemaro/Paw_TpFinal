0e0b334ea5e3486000e7e54641223129a6ae4b05
LucianoPal <luciano.palmieri.97@gmail.com>
2022-02-20 17:58:45 -0300

    modal function improvement

--- SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
+++ SegundaEntrega/InkMaster/public/js/gallery.modal.images.js
@@ -4,25 +4,34 @@ var window = window || {},
     id_tattoo_attribute,
     id_artist_attribute;
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Get the modal
-    var modal = document.getElementById("myModal"),
+document.addEventListener("DOMContentLoaded", () =>{
+    openModal();
+
+    const targetNode = document.body;
+    const config = { childList: true, subtree: true };
+
+    const observer = new MutationObserver(openModal);
+    observer.observe(targetNode, config);
+})
+
+function openModal() {
+    const items = document.querySelectorAll('.myImg'),
+        modal = document.getElementById("myModal"),
         modalNext = document.getElementsByClassName("modal-next")[0],
         modalPrevious = document.getElementsByClassName("modal-previous")[0],
-        modalClose = document.getElementsByClassName("close")[0],
-        modalImg = document.getElementById("img01"),
-        captionText = document.getElementById("caption");
+        modalClose = document.getElementsByClassName("close")[0];
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
-    document.querySelectorAll('.myImg').forEach(item => {
-        item.addEventListener('click', event => {
-            modal.style.display = "block";
-            modalImg.src = item.src;
-            captionText.innerHTML = item.alt;
-            id_tattoo_attribute = item.getAttribute( "data-tattoo-id" );
-            id_artist_attribute = item.getAttribute( "data-artist-id" );
-        })
-      })
+    items.forEach((item) => item.addEventListener("click", () =>{
+        var modalImg = document.getElementById("img01"),
+            captionText = document.getElementById("caption");
+
+        modal.style.display = "block";
+        modalImg.src = item.src;
+        captionText.innerHTML = item.alt;
+        id_tattoo_attribute = item.getAttribute( "data-tattoo-id" );
+        id_artist_attribute = item.getAttribute( "data-artist-id" );
+    }))
 
     // When the user clicks on <span> (x), close the modal
     modalClose.onclick = function() {
@@ -38,8 +47,7 @@ document.addEventListener("DOMContentLoaded", function() {
         modal.style.display = "block";
         changeImage( 'previous' );
     }
-
-});
+}
 
 function changeImage( action ) {
     var xmlhttp = new XMLHttpRequest(),
