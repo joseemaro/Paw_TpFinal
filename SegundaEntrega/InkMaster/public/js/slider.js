2a221ef8f67d190324ff496df6a9a414c1589a9d
Melany Bulfero <melanybulfero97@gmail.com>
2022-02-22 21:41:49 -0300

    Refact html and css
    - faq/list

--- SegundaEntrega/InkMaster/public/js/slider.js
+++ SegundaEntrega/InkMaster/public/js/slider.js
@@ -31,11 +31,11 @@ document.addEventListener("DOMContentLoaded", function() {
             slides[i].style.display = 'none';
         }
         for (i = 0; i < bars.length; i++) {
-            bars[i].className = bars[i].className.replace(" active", "");
+            bars[i].className = bars[i].className.replace(" active-slider", "");
         }
 
         slides[index - 1].style.display = 'block';
-        bars[index - 1].className += ' active';
+        bars[index - 1].className += ' active-slider';
 
     };
 });
